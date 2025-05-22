98dad5284c2ac4f2b028c0d7eb40ffd9dfb1ae9d
Chay Nabors <nabochay@amazon.com>
2025-05-21 17:11:01 -0700

    chore: Remove autocomplete (#1931)
    * remove autocomplete
    
    * fix cargo
    
    * remove typos config
    
    * remove mise and fix deny
    
    * fix tests

--- crates/aws-toolkit-telemetry-definitions/README.md
+++
@@ -1,3 +0,0 @@
-# aws-toolkit-telemetry-definitions
-
-https://github.com/aws/aws-toolkit-common/blob/main/telemetry/telemetryformat.md
--- crates/macos-utils/appkit-nsworkspace-bindings/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(improper_ctypes)]
-#![allow(dead_code)]
-#![allow(unexpected_cfgs)]
-#![allow(clippy::all)]
-#[cfg(target_os = "macos")]
-include!(concat!(env!("OUT_DIR"), "/nsworkspace.rs"));
